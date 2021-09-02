b328fb60523ae05427b46048a2ebc0f647639974
federojas <frojas@itba.edu.ar>
2021-09-02 11:43:59 -0300

    pruebas pipes

--- app.c
+++ app.c
@@ -1,14 +1,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 int main(int argc, char const *argv[])
 {
-    if(argc < 2) {
-        printf("Error: Invalid amount of arguments.\n");
-        exit(EXIT_FAILURE);
-    }
+    // if(argc < 2) {
+    //     printf("Error: Invalid amount of arguments.\n");
+    //     exit(EXIT_FAILURE);
+    // }
+    char* const* execArg = NULL;
+    int parent_to_child[2];
+    int child_to_parent[2];
+    pipe(parent_to_child);
+    pipe(child_to_parent);
+    int check = fork();
 
+    //child
+    if(check == 0) {
+        dup2(parent_to_child[0], 0);
+        dup2(child_to_parent[1], 1);
+
+        close(parent_to_child[0]);
+        close(parent_to_child[1]);
+        close(child_to_parent[0]);
+        close(child_to_parent[1]);
+
+        execv("./slave", execArg);
+    }
+    //parent
+    else {
+        check =  write(parent_to_child[1], "HOLA", 4); 
+        if(check == -1)
+            printf("Error");
+    
+        char buff[4];
+        check = read(child_to_parent[0], buff, 4);
+        if(check == -1)
+            printf("Error");
+        printf("%s\n", buff); //imprime (stdout)
+    }
+   
     return 0;
 }
 
--- slave.c
+++ slave.c
@@ -1,23 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    FILE * fp = fopen("prueba.txt", "r");
-    if(fp == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    char *line = NULL;
-    size_t len = 0;
-    while(getline(&line, &len, fp) != -1) {
-        fputs(line, stdout);
-    }
-
+    // FILE * fp = fopen("prueba.txt", "r");
+    // if(fp == NULL) {
+    //     exit(EXIT_FAILURE);
+    // }
+    // char *line = NULL;
+    // size_t len = 0;
+    // while(getline(&line, &len, fp) != -1) {
+    //     fputs(line, stdout);
+    //     printf("\n");
+    // }
     
-    fclose(fp);
-    free(line);
-
-    
-    
+    // fclose(fp);
+    // free(line);
+    char buff[4];
+   int check = read(0, buff, 4);
+   if(check == -1)
+            printf("Error");
+    printf("%s", buff); //se comunica con el parent (fd1 usa printf)
     return 0;
 }
