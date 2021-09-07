95238b9cd86421e44bc3d92d0c0b23bda6a66dd5
federojas <frojas@itba.edu.ar>
2021-09-07 17:18:06 -0300

    slave update
    borrado de comentarios, se reemplaza getline por for argc argv como indicado en clase 6/9

--- slave.c
+++ slave.c
@@ -7,47 +7,24 @@ void solve(char *line);
 
 int main(int argc, char const *argv[])
 {
-    /*
     //desactivamos buffer
     int check = setvbuf(stdout, NULL, _IONBF, 0);
     if(check != 0) {
         //ERROR HANDLER 
         printf("Error\n");
     }
-
-    // FILE * fp = fopen("prueba.txt", "r");
-    // if(fp == NULL) {
-    //     exit(EXIT_FAILURE);
-    // }
     
-    //https://www.youtube.com/watch?v=TKKQERrrt5o
-    char *line = NULL;
-    size_t len = 0;
-    while(getline(&line, &len, 0) != -1) {
-        solve(line);
-        // fputs(line, stdout);
-        // printf("\n");
+    for (size_t i = 1; i < argc; i++) {
+        solve((char *) argv[i]);
     }
-    //CHECKEAR ERRNO DEL GETLINE Y MANEJAR ERROR -1
     
-    // fclose(fp);
-    free(line);
-    */
-
-    
-//     char buff[4];
-//    int check = read(0, buff, 4);
-//    if(check == -1)
-//         printf("Error"); //aca no se imprime (pipe)
-//     printf("%s", buff); //se comunica con el parent (fd1 usa printf)
-
-    solve("hanoi4.cnf");
     return 0;
+
 }
 
 void solve(char *file){
-    char result[BUFF_SIZE]; 
-    char minisat_output[4096];
+    char result[BUFF_SIZE + 1]; 
+    char minisat_output[BUFF_SIZE + 1];
     
     int check = sprintf(result, "minisat %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", file);
     if(check == -1) {
@@ -59,7 +36,7 @@ void solve(char *file){
         printf("Error");
     }
     
-    int count = fread(minisat_output, sizeof(char), 4096, result_stream);
+    int count = fread(minisat_output, sizeof(char), BUFF_SIZE, result_stream);
 
     minisat_output[count] = 0;
 
