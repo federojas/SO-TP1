cb932159d680a9c7b01de6815c9b8eebe5b38fd1
ldagostino <ldagostino@itba.edu.ar>
2021-09-09 10:59:00 -0300

    Proceso vista

--- view.c
+++ view.c
@@ -3,5 +3,12 @@
 
 
 int main(int argc, char **argv) {
-    
+    if (argc == 1) {
+        // ./vista invocado en un pipe (./solve files/* | ./vista)
+    } else if (argc == 2) {
+        // ./vista invocado con informacion ( ./vista <info> )
+    } else {
+        fprintf(stderr, "Usage: < %s > or < %s >", "./solve files/* | ./vista", "./vista <info>");
+        exit(EXIT_FAILURE);
+    }
 }
