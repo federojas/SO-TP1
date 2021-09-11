c107a2c8cd9957f8a61362d43c75cb0a5a6e2e1d
ldagostino <ldagostino@itba.edu.ar>
2021-09-11 10:44:00 -0300

    view updated

--- view.c
+++ view.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <semaphore.h>
 
+#define SHM_NAME "/shm"
 
 int main(int argc, char **argv) {
+    int total_tasks;
+
     if (argc == 1) {
         // ./vista invocado en un pipe (./solve files/* | ./vista)
     } else if (argc == 2) {
@@ -10,5 +14,16 @@ int main(int argc, char **argv) {
     } else {
         fprintf(stderr, "Usage: < %s > or < %s >", "./solve files/* | ./vista", "./vista <info>");
         exit(EXIT_FAILURE);
+    }
+
+    sharedDataADT data = openSharedData();
+    sem_t *full_sem = get_full(data);
+    sem_t *mutex_sem = get_mutex(data);
+    char *shm_base = get_shm_base(data);
+
+    int i = 0;
+    while (i < total_tasks) {
+        sem_wait(full_sem);
+        sem_wait(mutex_sem);
     }
 }
